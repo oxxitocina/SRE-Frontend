8d6db82b461669d77855c6851c6e0bcea124adb0
oxxitocina <U2HDxO@protonmail.com>
2023-11-21 11:09:43 +0600

    test

--- src/helpers/mathFunction.test.ts
+++ src/helpers/mathFunction.test.ts
@@ -3,7 +3,7 @@ import { add } from "./mathFunctions";
 test('add function adds two numbers correctly', () => {
   expect(add(2, 3)).toBe(5);
   expect(add(-1, 1)).toBe(0);
-  expect(add(0, 0)).toBe(1);
+  expect(add(0, 0)).toBe(0);
 });
 
 describe('add function integration tests', () => {
